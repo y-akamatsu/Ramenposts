8ec8b42312b5f689770cc47ca48880caf51f5dd8
y.akamatsu <git config --global user.email>
2018-07-21 14:16:17 +0000

    kadai3

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -5,13 +5,13 @@ class LikesController < ApplicationController
     ramenpost = Ramenpost.find(params[:ramenpost_id])
     current_user.like(ramenpost)
     flash[:success] = 'likeを追加しました。'
-    redirect_to likes_user_path(current_user)
+    redirect_back(fallback_location: root_path)
   end
 
   def destroy
     ramenpost = Ramenpost.find(params[:ramenpost_id])
     current_user.unlike(ramenpost)
     flash[:success] = 'likeを解除しました。'
-    redirect_to likes_user_path(current_user)
+    redirect_back fallback_location: root_path
   end
 end
