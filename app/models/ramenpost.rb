12d0df32472acd61e1c9cfee2cb35375eeec8422
y.akamatsu <git config --global user.email>
2018-07-23 11:10:54 +0000

    last

--- app/models/ramenpost.rb
+++ app/models/ramenpost.rb
@@ -6,7 +6,8 @@ class Ramenpost < ApplicationRecord
   validates :content, presence: true, length: { maximum: 255 }
   validates :menu, presence: true, length: { maximum: 20 }
   validates :restaurant_name, presence: true, length: { maximum: 30 }
-
+  validates :image, presence: true
+  
   has_many :likes, dependent: :destroy
   has_many :evaluations, dependent: :destroy
 end
